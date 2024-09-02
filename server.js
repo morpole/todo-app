b0a4ebb032d191971ea84418057e21fbc88fb299
morpole <morpole@gmail.com>
2024-09-02 18:21:54 +0100

    added signup using Auth0

--- server.js
+++ server.js
@@ -26,3 +26,22 @@ const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+const { auth } = require('express-openid-connect');
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: '2f36d107b0ef4aca54eae4706ce55a048859ca5fff9d1ef868d94872e8c5b0ac',
+  baseURL: 'http://localhost:3000',
+  clientID: 'v9junef7n55fMfUVVl5pC89jMAQXlUyS',
+  issuerBaseURL: 'https://dev-603hvls21rfemblu.eu.auth0.com'
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
